6bbb6e8c7f9913b137e6b486df9c65710519025d
MahmoudAlla88 <hamoodaalla88@gmail.com>
2025-02-11 10:18:39 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# project-redux-booking
+# Redux-Project
+- [Trello Board](https://trello.com/b/NC0k2kSM/rental-project)
+- [Figma Design](https://www.figma.com/design/17pHzmDQKjeGw4euGj1HTB/Untitled?node-id=0-1&p=f&t=vxM4QgHEXuJJM26r-0)
---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar"
+import Footer from "./components/footer"
+import Home from "./components/Home/Home"
+import Aboutus from "./components/AboutUs/Aboutus"
+import Contactus from "./components/ContactUs/Contactus"
+// import SignUp from "./components/SignUp/SignUp"
+import LoginPage from "./components/LogIn/Login"
+import SignUp from "./components/SignUp/SignUp"
+
+
+
+function App() {
+
+  return (
+      // <Router>
+      //   <Navbar />
+      //   <Routes>
+      //     <Route path="/" element = {<Home />}></Route>
+      //     <Route path="/aboutUs" element = {<Aboutus />}></Route>
+      //     <Route path="/contactUs" element = {<Contactus />}></Route>
+      //   </Routes>
+      //   <Footer />
+      // </Router>
+      // <LoginPage/>
+      <Router>
+      <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes></Router>
+  )
+}
+
+export default App
